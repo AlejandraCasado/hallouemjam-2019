a64c68519947a770ec42efd091db5f1b416509b5
alejandracc <alejandracasado97@gmail.com>
2019-10-26 18:10:55 +0200

    cosas

---
+++ UnityProject/Assets/Scripts/script_play.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_buttons : MonoBehaviour
+{
+    public Button mybutton;
+    public Sprite simpleButton;
+    public Sprite button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
