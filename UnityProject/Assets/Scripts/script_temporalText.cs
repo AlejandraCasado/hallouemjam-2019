fb713d5d1d35df46579f12be1440d6919b3a9aeb
Pedro Casas Martínez <pedrocasas876@gmail.com>
2019-10-26 19:31:01 +0200

    outdoors3

---
+++ UnityProject/Assets/Scripts/script_temporalText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_temporalText : MonoBehaviour
+{
+    float time;
+    Text
+
+    //public void setActiveForXSeconds(float x)
+    //{
+    //    time = x;
+    //}
+
+    //IEnumerator end
+}
