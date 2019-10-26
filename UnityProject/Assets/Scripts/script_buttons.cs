fab61647b0da9846e5e4197e8fd1f2930d50c29a
alejandracc <alejandracasado97@gmail.com>
2019-10-26 16:14:48 +0200

    interfaz 2.0

--- UnityProject/Assets/Scripts/script_buttons.cs
+++ UnityProject/Assets/Scripts/script_buttons.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class script_buttons : MonoBehaviour
 {
+    public Button mybutton;
+    public Sprite simpleButton;
+    public Sprite button;
     // Start is called before the first frame update
     void Start()
     {
