32f1730cb4d1e8eedd460a46dc9fe01b09290a64
alejandracc <alejandracasado97@gmail.com>
2019-10-26 14:36:25 +0200

    cosas random

---
+++ UnityProject/Assets/Scripts/script_quit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_quit : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
