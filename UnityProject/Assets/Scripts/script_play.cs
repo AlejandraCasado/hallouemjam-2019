0876181789a8fc9b3a24575874ff72479cbc33cf
alejandracc <alejandracasado97@gmail.com>
2019-10-26 18:24:08 +0200

    interfaz fetén

--- UnityProject/Assets/Scripts/script_play.cs
+++ UnityProject/Assets/Scripts/script_play.cs
@@ -3,20 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class script_buttons : MonoBehaviour
+public class script_play : MonoBehaviour
 {
-    public Button mybutton;
-    public Sprite simpleButton;
-    public Sprite button;
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeMenusScene(string nameScene)
     {
-        
+        Application.LoadLevel(nameScene);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
